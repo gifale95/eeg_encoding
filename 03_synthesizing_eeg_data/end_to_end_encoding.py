bfe334d68e32761fb2103428e50c76f2a608fb30
gifale95 <alessandro.gifford@gmail.com>
2022-08-16 11:03:53 +0200

    Added validation set

--- 03_synthesizing_eeg_data/end_to_end_encoding.py
+++ 03_synthesizing_eeg_data/end_to_end_encoding.py
@@ -1,21 +1,15 @@
-"""Training end-to-end DNN models which predict the EEG responses to the test
-images. The EEG responses to each test image condition are synthesized in a
-cross-validated fashion, by using the training epoch weights which yielded the
-highest correlation prediction score for all the other N-1 test image
-conditions.
+"""Train DNN models end-to-end to predict the EEG responses to arbitrary images.
 
 Parameters
 ----------
 sub : int
 	Used subject.
-tot_eeg_chan : int
-	Total amount of EEG channels.
-tot_eeg_time : int
-	Total amount of EEG time points.
+modeled_time_points : str
+	Whether each DNN should model 'single' or 'all' EEG time points.
 dnn : str
 	DNN model used.
 pretrained : bool
-	Whether to use or not pretrained DNN models.
+	If True use a pretrained network, if False use a randomly initialized one.
 epochs : int
 	Number of training epochs.
 lr : float
@@ -24,6 +18,8 @@ weight_decay : float
 	Weight decay coefficient.
 batch_size : int
 	Batch size for weight update.
+save_trained_models : bool
+	If True, the weights of the trained models are saved.
 project_dir : str
 	Directory of the project folder.
 
@@ -34,22 +30,25 @@ import os
 import numpy as np
 import random
 import torch
+import torchvision
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+from copy import deepcopy
 from end_to_end_encoding_utils import load_images
 from end_to_end_encoding_utils import load_eeg_data
 from end_to_end_encoding_utils import create_dataloader
-from end_to_end_encoding_utils import get_model
-from scipy.stats import pearsonr as corr
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--sub', type=int, default=1)
-parser.add_argument('--tot_eeg_chan', type=int, default=17)
-parser.add_argument('--tot_eeg_time', type=int, default=100)
+parser.add_argument('--modeled_time_points', type=str, default='single')
 parser.add_argument('--dnn', type=str, default='alexnet')
 parser.add_argument('--pretrained', type=bool, default=False)
-parser.add_argument('--epochs', type=int, default=30)
-parser.add_argument('--lr', type=float, default=0.0001)
-parser.add_argument('--weight_decay', type=float, default=0.001)
-parser.add_argument('--batch_size', type=int, default=256)
+parser.add_argument('--epochs', type=int, default=50)
+parser.add_argument('--lr', type=float, default=1e-7)
+parser.add_argument('--weight_decay', type=float, default=0.)
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--save_trained_models', type=bool, default=False)
 parser.add_argument('--project_dir', default='/project/directory', type=str)
 args = parser.parse_args()
 
@@ -73,128 +72,197 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 # =============================================================================
-# Loading the images (X) and the EEG data (y)
+# Load the images (X) and the EEG data (y)
 # =============================================================================
-X_train, X_test = load_images(args)
+# One image exemplar for each of the 1,654 training object concepts is used for
+# model validation, for a total of 1,654 validation images.
+# The remaining 14,886 images (1,654 training object concepts × 9 image
+# exemplars per concept) are used for training the encoding models.
+# Finally, the 200 test object concepts are used for testing the models.
+
+# Load the images
+X_train, X_val, X_test = load_images(args)
+
+# Load the EEG data
+y_train, y_val, y_test, ch_names, times = load_eeg_data(args)
+
+
+# =============================================================================
+# Models loop
+# =============================================================================
+# Loop across the EEG temporal dimension if the encoding models are trained
+# independently on each time point.
+# If the encoding models are predicting all time points at once, then the loop
+# will only run for one iteration.
+
+# Define the amount of models which will be trained and the amount of their EEG
+# output features
+if args.modeled_time_points == 'single':
+	num_models = y_test.shape[2]
+	out_features = y_test.shape[1]
+elif args.modeled_time_points == 'all':
+	num_models = 1
+	out_features = y_test.shape[1] * y_test.shape[2]
 
 # Snthetic EEG data matrix of shape:
 # (Test image conditions × EEG channels × EEG time points)
-synthetic_data = np.zeros((len(X_test),args.tot_eeg_chan,args.tot_eeg_time))
-# Loop across EEG time points
-for t in range(args.tot_eeg_time):
-	print(f'\nTime: [{t+1}/{args.tot_eeg_time}]')
-	y_train, y_test, ch_names, times = load_eeg_data(args, t)
+synthetic_data = np.zeros((y_test.shape))
+best_epochs = np.zeros((num_models))
+
+# Loop across independent models
+for m in range(num_models):
+	print(f'\nModel: [{m+1}/{num_models}]')
 
 
 # =============================================================================
-# Creating PyTorch-compatible training and test Dataloaders
+# Create PyTorch-compatible Dataloaders
 # =============================================================================
-	train_dl, test_dl = create_dataloader(args, X_train, y_train, X_test,
-		y_test)
-	del y_train, y_test
+	train_dl, val_dl, test_dl = create_dataloader(args, m, X_train, X_val,
+		X_test, y_train, y_val, y_test)
 
 
 # =============================================================================
-# Loading the DNN model and changing the last layer to 17 features
+# Load the DNN model and change the last layer to the amount of EEG features
 # =============================================================================
-	model = get_model(args)
+	# AlexNet
+	if args.dnn == 'alexnet':
+		model = torchvision.models.alexnet(pretrained=args.pretrained)
+		model.classifier[6] = nn.Linear(in_features=4096,
+			out_features=out_features)
+	# ResNet-50
+	if args.dnn == 'resnet50':
+		model = torchvision.models.resnet50(pretrained=args.pretrained)
+		model.fc = nn.Linear(in_features=2048, out_features=out_features)
 	model.to(device)
 
 
 # =============================================================================
-# Defining loss function, optimizer, training and test loops
+# TensorBoard, loss function and optimizer
 # =============================================================================
+	# TensorBoard
+	save_dir = os.path.join(args.project_dir, 'results', 'sub-'+
+		format(args.sub,'02'), 'synthetic_eeg_data', 'end_to_end_encoding',
+		'dnn-'+args.dnn, 'modeled_time_points-'+args.modeled_time_points,
+		'pretrained-'+str(args.pretrained), 'lr-{:.0e}'.format(args.lr)+
+		'__wd-{:.0e}'.format(args.weight_decay)+'__bs-'+
+		format(args.batch_size,'03'))
+	if os.path.isdir(save_dir) == False:
+		os.makedirs(save_dir)
+	writer = SummaryWriter(save_dir)
+
 	# Loss function
-	loss_fn = torch.nn.MSELoss(reduction='sum')
+	loss_fn = torch.nn.MSELoss().to(device)
 
 	# Optimizer
-	optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,
+	optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr,
 		weight_decay=args.weight_decay)
 
-	# Training loop
+
+# =============================================================================
+# Training and evaluation loops
+# =============================================================================
 	def train_loop(train_dl, model, loss_fn, optimizer):
+		tot_loss = 0
 		model.train()
-		for batch, (X, y) in enumerate(train_dl):
-			X, y = X.to(device), y.to(device)
-			# Compute prediction and loss
-			pred = model(X).squeeze()
-			loss = loss_fn(pred, y)
+		for X, y in train_dl:
+			# Prediction and loss
+			X = X.to(device, non_blocking=False)
+			y = y.to(device, non_blocking=False)
+			y_pred = model(X).squeeze()
+			loss = loss_fn(y_pred, y)
 			# Backpropagation
 			optimizer.zero_grad()
 			loss.backward()
 			optimizer.step()
+			# Total loss
+			tot_loss += loss.item() * len(X)
+		tot_loss /= len(train_dl.dataset)
+		return tot_loss
 
-	# Test loop
-	def test_loop(test_dl, model, corr_results):
+	def eval_loop(eval_dl, model, loss_fn):
+		tot_loss = 0
 		model.eval()
 		with torch.no_grad():
-			for X, y in test_dl:
-				X, y = X.to(device), y.to(device)
-				pred = model(X).squeeze()
-				# Performing the correlation in a leave-one-image-condition-out
-				# fashion for cross-validation
-				corr_mean = []
-				for i in range(pred.shape[0]):
-					pred_cond = np.delete(pred.cpu().numpy(), i, 0)
-					y_cond = np.delete(y.cpu().numpy(), i, 0)
-					correlation = []
-					for c in range(pred_cond.shape[1]):
-						correlation.append(corr(pred_cond[:,c], y_cond[:,c])[0])
-					corr_mean.append(np.mean(correlation))
-		corr_results.append(corr_mean)
+			for X, y in eval_dl:
+				# Prediction and loss
+				X = X.to(device, non_blocking=False)
+				y = y.to(device, non_blocking=False)
+				y_pred = model(X).squeeze()
+				loss = loss_fn(y_pred, y)
+				tot_loss += loss.item() * len(X)
+			tot_loss /= len(eval_dl.dataset)
+		return tot_loss
 
 
 # =============================================================================
-# Training the model
+# Train the model and log the training statistics to TensorBoard
 # =============================================================================
-	model_epochs_weights = []
-	corr_results = []
-	for e in range(args.epochs):
-		model.to(device)
-		train_loop(train_dl, model, loss_fn, optimizer)
-		test_loop(test_dl, model, corr_results)
-		# Saving the model's weights at each training epoch
-		model_epochs_weights.append(model.cpu())
+	best_val_loss = 5000
+
+	for e in tqdm(range(args.epochs)):
+		# Train the model and train loss
+		train_loss = train_loop(train_dl, model, loss_fn, optimizer)
+		# Validation and test losses
+		val_loss = eval_loop(val_dl, model, loss_fn)
+		test_loss = eval_loop(test_dl, model, loss_fn)
+		# Add the training stats to TensorBoard
+		writer.add_scalars('Loss', {'train': train_loss, 'val': val_loss,
+			'test': test_loss,}, e+1)
+		writer.flush()
+		# Retain the best epoch model according to the validation loss
+		if val_loss < best_val_loss:
+			best_model = deepcopy(model)
+			best_epochs[m] = e + 1
+			best_val_loss = val_loss
+
+	# Delete the model from GPU memory
 	del model
+	if device == 'cuda':
+		torch.cuda.empty_cache()
 
 
 # =============================================================================
-# Synthesizing the EEG data
+# Synthesize the EEG data using the best model
 # =============================================================================
-	# Finding the best epoch for each test image condition
-	corr_results = np.asarray(corr_results)
-	best_epoch_idx = np.argsort(corr_results, 0)[::-1][0]
-
-	# Synthesizing the EEG data response to each test image codition using the
-	# epoch weights which best synthesized all other test image conditions, as
-	# measured through the correlation score
+	best_model.to(device)
 	with torch.no_grad():
 		for X, y in test_dl:
-			for i in range(len(best_epoch_idx)):
-				X_cond = X[i:i+1]
-				X_cond = X_cond.to(device)
-				model = model_epochs_weights[best_epoch_idx[i]]
-				model.to(device)
-				model.eval()
-				synthetic_data[i,:,t] = model(X_cond).squeeze().cpu().numpy()
-				del model
-	del model_epochs_weights
+			best_model.eval()
+			if args.modeled_time_points == 'single':
+				synthetic_data[:,:,m] = best_model(X).detach().cpu().numpy()
+			elif args.modeled_time_points == 'all':
+				synthetic_data = np.reshape(
+					best_model(X).detach().cpu().numpy(),
+					(synthetic_data.shape))
 
 
 # =============================================================================
-# Saving the synthesized EEG data
+# Save the best model
 # =============================================================================
-data_dict_test = {
-	'synthetic_data' : synthetic_data,
-	'ch_names' : ch_names,
-	'times': times
+	if args.save_trained_models == True:
+		file_name = 'model_state_dict.pt'
+		best_model = best_model.to('cpu')
+		torch.save({
+			'args': args,
+			'best_model': best_model.state_dict(),
+			'out_features': out_features,
+			'best_epochs': best_epochs
+			}, os.path.join(save_dir, file_name))
+
+	# Delete the model from GPU memory
+	del best_model
+	if device == 'cuda':
+		torch.cuda.empty_cache()
+
+
+# =============================================================================
+# Save the modeling results
+# =============================================================================
+modeling_results = {
+	'synthetic_data': synthetic_data,
+	'ch_names': ch_names,
+	'times': times,
+	'best_epochs': best_epochs
 }
-
-save_dir = os.path.join(args.project_dir, 'results', 'sub-'+
-	format(args.sub,'02'), 'synthetic_eeg_data', 'end_to_end_encoding',
-	'dnn-'+args.dnn)
-file_name_test = 'synthetic_eeg_test'
-
-if not os.path.exists(save_dir):
-	os.makedirs(save_dir)
-np.save(os.path.join(save_dir, file_name_test), data_dict_test)
+file_name = 'synthetic_eeg_test'
+np.save(os.path.join(save_dir, file_name), modeling_results)
--- 03_synthesizing_eeg_data/end_to_end_encoding_utils.py
+++ 03_synthesizing_eeg_data/end_to_end_encoding_utils.py
@@ -1,5 +1,5 @@
 def load_images(args):
-	"""Loading and preprocessing the training and test images.
+	"""Load and preprocess the training, validation and test images.
 
 	Parameters
 	----------
@@ -10,6 +10,8 @@ def load_images(args):
 	-------
 	X_train : list of tensor
 		Training images.
+	X_val : list of tensor
+		Validation images.
 	X_test : list of tensor
 		Test images.
 
@@ -20,14 +22,14 @@ def load_images(args):
 	from tqdm import tqdm
 	from PIL import Image
 
-	### Defining the image preprocesing ###
+	### Define the image preprocesing ###
 	preprocess = transforms.Compose([
 		transforms.Resize((224,224)),
 		transforms.ToTensor(),
 		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 	])
 
-	### Loading and preprocessing the training images ###
+	### Load and preprocess the training and validation images ###
 	img_dirs = os.path.join(args.project_dir, 'image_set',
 		'training_images')
 	image_list = []
@@ -37,12 +39,18 @@ def load_images(args):
 				image_list.append(os.path.join(root,file))
 	image_list.sort()
 	X_train = []
-	for image in tqdm(image_list):
+	X_val = []
+	for i, image in enumerate(tqdm(image_list)):
 		img = Image.open(image).convert('RGB')
 		img = preprocess(img)
-		X_train.append(img)
+		# Use the first image exemplar of each training object concept for
+		# validation
+		if i % 10 == 0:
+			X_val.append(img)
+		else:
+			X_train.append(img)
 
-	### Loading and preprocessing the test images ###
+	### Load and preprocess the test images ###
 	img_dirs = os.path.join(args.project_dir, 'image_set',
 		'test_images')
 	image_list = []
@@ -58,24 +66,23 @@ def load_images(args):
 		X_test.append(img)
 
 	### Output ###
-	return X_train, X_test
+	return X_train, X_val, X_test
 
 
-def load_eeg_data(args, eeg_time):
-	"""Loading the EEG training and test data, and selecting only the desired
-	channel and time point.
+def load_eeg_data(args):
+	"""Load the EEG training, validation and test data.
 
 	Parameters
 	----------
 	args : Namespace
 		Input arguments.
-	eeg_time : int
-		EEG time point used.
 
 	Returns
 	-------
 	y_train : float
 		Training EEG data.
+	y_val : float
+		Validation EEG data.
 	y_test : float
 		Test EEG data.
 	ch_names : list of str
@@ -87,8 +94,9 @@ def load_eeg_data(args, eeg_time):
 
 	import os
 	import numpy as np
+	import torch
 
-	### Loading the EEG training data ###
+	### Load the EEG training data ###
 	data_dir = os.path.join('eeg_dataset', 'preprocessed_data', 'sub-'+
 		format(args.sub,'02'))
 	training_file = 'preprocessed_eeg_training.npy'
@@ -97,43 +105,52 @@ def load_eeg_data(args, eeg_time):
 	y_train = data['preprocessed_eeg_data']
 	ch_names = data['ch_names']
 	times = data['times']
-	# Selecting the EEG time point
-	y_train = y_train[:,:,:,eeg_time]
-	# Averaging across repetitions
+	# Average across repetitions
 	y_train = np.mean(y_train, 1)
-	# Converting to float32 (for DNN training with Pytorch)
-	y_train = np.float32(y_train)
 
-	### Loading the EEG test data ###
+	### Create the validation partition from the training data ###
+	idx_val = np.arange(0, len(y_train), 10)
+	y_val = y_train[idx_val]
+	y_train = np.delete(y_train, idx_val, 0)
+	# Convert the data to tensor format
+	y_val = torch.tensor(np.float32(y_val))
+	y_train = torch.tensor(np.float32(y_train))
+
+	### Load the EEG test data ###
 	test_file = 'preprocessed_eeg_test.npy'
 	data = np.load(os.path.join(args.project_dir, data_dir, test_file),
 		allow_pickle=True).item()
 	y_test = data['preprocessed_eeg_data']
-	# Selecting the EEG time point
-	y_test = y_test[:,:,:,eeg_time]
-	# Averaging across repetitions
+	# Average across repetitions
 	y_test = np.mean(y_test, 1)
-	# Converting to float32 (for DNN training with Pytorch)
-	y_test = np.float32(y_test)
+	# Convert the data to tensor format
+	y_test = torch.tensor(np.float32(y_test))
 
 	### Output ###
-	return y_train, y_test, ch_names, times
+	return y_train, y_val, y_test, ch_names, times
 
 
-def create_dataloader(args, X_train, y_train, X_test, y_test):
-	"""Putting the training and test data into a PyTorch-compatible Dataloader
-	format.
+def create_dataloader(args, time_point, X_train, X_val, X_test, y_train, y_val,
+	y_test):
+	"""Put the training, validation and test data into a PyTorch-compatible
+	Dataloader format.
 
 	Parameters
 	----------
 	args : Namespace
 		Input arguments.
+	time_point : int
+		Modeled EEG time point.
 	X_train : list of tensor
 		Training images.
-	y_train : float
-		Training EEG data.
+	X_val : list of tensor
+		Validation images.
 	X_test : list of tensor
 		Test images.
+	y_train : float
+		Training EEG data.
+	y_val : float
+		Validation EEG data.
 	y_test : float
 		Test EEG data.
 
@@ -141,75 +158,54 @@ def create_dataloader(args, X_train, y_train, X_test, y_test):
 	----------
 	train_dl : Dataloader
 		Training Dataloader.
+	val_dl : Dataloader
+		Validation Dataloader.
 	test_dl : Dataloader
 		Test Dataloader.
 
 	"""
 
+	import torch
 	from torch.utils.data import Dataset
 	from torch.utils.data import DataLoader
 
 	### Dataset class ###
 	class EegDataset(Dataset):
-		def __init__(self, X_train, y_train, transform=None,
-				target_transform=None):
-			self.X_train = X_train
-			self.y_train = y_train
+		def __init__(self, X, y, modeled_time_points, transform=None,
+			target_transform=None):
+			self.modeled_time_points = modeled_time_points
+			self.X = X_train
+			if self.modeled_time_points == 'single':
+				self.y = y[:,:,time_point]
+			elif self.modeled_time_points == 'all':
+				self.y = torch.reshape(y, (y.shape[0],-1))
 			self.transform = transform
 			self.target_transform = target_transform
 
 		def __len__(self):
-			return len(self.y_train)
+			return len(self.y)
 
 		def __getitem__(self, idx):
-			image = self.X_train[idx]
-			target = self.y_train[idx]
+			image = self.X[idx]
+			target = self.y[idx]
 			if self.transform:
 				image = self.transform(image)
 			if self.target_transform:
 				target = self.target_transform(target)
 			return image, target
 
-	### Converting the data to PyTorch's Dataset format ###
-	train_ds = EegDataset(X_train, y_train)
-	test_ds = EegDataset(X_test, y_test, target_transform=None)
+	### Convert the data to PyTorch's Dataset format ###
+	train_ds = EegDataset(X_train, y_train, args.modeled_time_points)
+	val_ds = EegDataset(X_val, y_val, args.modeled_time_points)
+	test_ds = EegDataset(X_test, y_test, args.modeled_time_points)
 
-	### Converting the Datasets to PyTorch's Dataloader format ###
-	train_dl = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True)
-	test_dl = DataLoader(test_ds, batch_size=test_ds.__len__(), shuffle=False)
-
-	### Output ###
-	return train_dl, test_dl
-
-
-def get_model(args):
-	"""Loading the DNN model, and changing the last layer to 17 features, one
-	for each EEG channel.
-
-	Parameters
-	----------
-	args : Namespace
-		Input arguments.
-
-	Returns
-	----------
-	model : PytorchModel
-		DNN model.
-
-	"""
-
-	import torchvision
-	import torch.nn as nn
-
-	### Alexnet ###
-	if args.dnn == 'alexnet':
-		model = torchvision.models.alexnet(pretrained=args.pretrained)
-		model.classifier[6] = nn.Linear(in_features=4096, out_features=17)
-
-	### ResNet-50 ###
-	if args.dnn == 'resnet50':
-		model = torchvision.models.resnet50(pretrained=args.pretrained)
-		model.fc = nn.Linear(in_features=2048, out_features=17)
+	### Convert the Datasets to PyTorch's Dataloader format ###
+	train_dl = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True,
+		pin_memory=True)
+	val_dl = DataLoader(val_ds, batch_size=val_ds.__len__(), shuffle=False,
+		pin_memory=True)
+	test_dl = DataLoader(test_ds, batch_size=test_ds.__len__(), shuffle=False,
+		pin_memory=True)
 
 	### Output ###
-	return model
+	return train_dl, val_dl, test_dl
